288a9b49bd6cd6e1818b4bdac09ea2e1cdc8c691
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2022-07-06 20:58:30 +0100

    gdeploy: use 'create' and 'update' commands

---
+++ cmd/gdeploy/commands/create.go
@@ -0,0 +1,52 @@
+package commands
+
+import (
+	"github.com/common-fate/granted-approvals/pkg/clio"
+	"github.com/common-fate/granted-approvals/pkg/deploy"
+	"github.com/urfave/cli/v2"
+)
+
+var CreateCommand = cli.Command{
+	Name:        "create",
+	Usage:       "Create a new Granted Approvals deployment by deploying CloudFormation",
+	Description: "Create a new Granted Approvals deployment based on a deployment configuration file (granted-deployment.yml by default). Deploys resources to AWS using CloudFormation.",
+	Flags: []cli.Flag{
+		&cli.BoolFlag{Name: "confirm", Usage: "if provided, will automatically deploy without asking for confirmation"},
+	},
+	Action: func(c *cli.Context) error {
+		ctx := c.Context
+
+		f := c.Path("file")
+
+		dc, err := deploy.LoadConfig(f)
+		if err != nil {
+			return err
+		}
+
+		clio.Info("Deploying Granted Approvals %s", dc.Deployment.Release)
+		clio.Info("Using CloudFormation template: %s", dc.CfnTemplateURL())
+		confirm := c.Bool("confirm")
+		err = dc.DeployCloudFormation(ctx, confirm)
+		if err != nil {
+			return err
+		}
+		o, err := dc.LoadOutput(ctx)
+
+		if err != nil {
+			return err
+		}
+		o.PrintTable()
+
+		clio.Success("Your Granted deployment has been created")
+		clio.Info(`Here are your next steps to get started:
+
+  1) create an admin user so you can log in: 'gdeploy user create --admin -u YOUR_EMAIL_ADDRESS'
+  2) add an Access Provider: 'gdeploy provider add'
+  3) visit the web dashboard: 'gdeploy dashboard open'
+
+Check out the next steps in our getting started guide for more information: https://docs.comonfate.io/granted-approvals/getting-started/deploying
+`)
+
+		return nil
+	},
+}
