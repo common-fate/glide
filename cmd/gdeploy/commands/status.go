e7d94bda07466a2bfafb3174078afeda343c82a6
Joshua Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-07-11 10:11:35 +0800

    Remove redundant error log

--- cmd/gdeploy/commands/status.go
+++ cmd/gdeploy/commands/status.go
@@ -4,23 +4,16 @@ import (
 	"github.com/common-fate/granted-approvals/pkg/clio"
 	"github.com/common-fate/granted-approvals/pkg/deploy"
 	"github.com/urfave/cli/v2"
-	"go.uber.org/zap"
 )
 
 var StatusCommand = cli.Command{
 	Name:        "status",
 	Description: "Check the status of a Granted deployment",
 	Action: func(c *cli.Context) error {
-		log := zap.S()
-
 		ctx := c.Context
 
 		f := c.Path("file")
 		dc, err := deploy.LoadConfig(f)
-		if err == deploy.ErrConfigNotExist {
-			log.Errorf("couldn't find config file %s")
-			return nil
-		}
 		if err != nil {
 			return err
 		}
