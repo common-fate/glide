b315772eabbc956ab4ea7df3acee579ac1f12712
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2022-07-12 03:35:54 +0100

    Add process for release candidate releasing (#15)
    * add docs on releasing
    
    * release to granted-test-releases-us-east-1
    
    bucket is public which simplifies RC deployment testing
    separate 'dev' and 'rc' folders
    dev folder is for each commit
    rc folder is for RCs only
    
    * update CI workflow file
    
    * swap assume role region to us-east-1
    
    * gdeploy: support custom release URLs
    
    this allows us to point to release candidate template URLs easily
    
    * swap release region to us-west-2
    
    to match our production releases
    
    * Update .github/workflows/deploy.yml
    
    Co-authored-by: JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>

---
+++ docs/releasing.md
@@ -0,0 +1,15 @@
+# Granted Approvals releases
+
+Our versioning follows [SemVer numbering](https://semver.org/). Currently both `gdeploy` and our CloudFormation templates are versioned and released together. In future, this may change if it's more convenient to use separate release cycles for each one.
+
+Prior to a release being made, we create a 'Release Candidate' (RC) version. The tagging format of an RC is `<NEXT_VERSION>-rc<RC_NUMBER>`, where `NEXT_VERSION` is the version we are planning to release (e.g. `v0.2.0`) and `RC_NUMBER` is a number starting at 1 which is incremented for each subsequent RC.
+
+An example of an RC tag is `v0.2.0-rc2`.
+
+The RC is released to the `granted-test-releases-us-east-1` S3 bucket. The CloudFormation template can be found at:
+
+```
+s3://granted-test-releases-us-east-1/rc/<TAG>/Granted.template.json
+```
+
+Where `<TAG>` is the RC tag, e.g. `v0.2.0-rc2`.
--- pkg/deploy/config_test.go
+++ pkg/deploy/config_test.go
@@ -122,3 +122,52 @@ func TestUnmarshalIdentity(t *testing.T) {
 		})
 	}
 }
+
+func TestCfnTemplateURL(t *testing.T) {
+	type testcase struct {
+		name string
+		give Config
+		want string
+	}
+
+	testcases := []testcase{
+		{
+			name: "version tag",
+			give: Config{
+				Deployment: Deployment{
+					Region:  "ap-southeast-2",
+					Release: "v0.1.0",
+				},
+			},
+			want: "https://granted-releases-ap-southeast-2.s3.amazonaws.com/v0.1.0/Granted.template.json",
+		},
+		{
+			name: "custom URL",
+			give: Config{
+				Deployment: Deployment{
+					Region:  "ap-southeast-2",
+					Release: "https://custom-release.s3.amazonaws.com/template.json",
+				},
+			},
+			want: "https://custom-release.s3.amazonaws.com/template.json",
+		},
+		{
+			// note: this currently won't return an error, even though CloudFormation will refuse to deploy it.
+			// this test captures this behaviour - in future we can add more validation around the URL.
+			name: "custom URL not in S3",
+			give: Config{
+				Deployment: Deployment{
+					Release: "https://some-other-url.com",
+				},
+			},
+			want: "https://some-other-url.com",
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.give.CfnTemplateURL()
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
