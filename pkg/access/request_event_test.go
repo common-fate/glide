a5896d35642f9f010ea23775b18baf6ab4c42be1
Joshua Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-07-15 13:35:28 +0800

    fix test

--- pkg/access/request_event_test.go
+++ pkg/access/request_event_test.go
@@ -3,7 +3,6 @@ package access
 import (
 	"encoding/json"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -11,31 +10,18 @@ import (
 func TestRequestEventMarshalDDB(t *testing.T) {
 	type testcase struct {
 		name string
-		give Request
+		give RequestEvent
 		want string
 	}
-
-	reason := "test reason"
 
 	testcases := []testcase{
 		{
 			name: "basic",
-			give: Request{
-				ID:          "req_28w2Eebt2Q8nFQJ2dKa1FTE9X0J",
-				RequestedBy: "user",
-				Rule:        "rul_123",
-				RuleVersion: "2022-01-01T10:00:00Z",
-				Status:      "PENDING",
-				Data: RequestData{
-					Reason: &reason,
-				},
-				RequestedTiming: Timing{
-					Duration: time.Minute * 5,
-				},
-				CreatedAt: time.Date(2022, 1, 1, 10, 0, 0, 0, time.UTC),
-				UpdatedAt: time.Date(2022, 1, 1, 10, 0, 0, 0, time.UTC),
+			give: RequestEvent{
+				ID:        "his_28w2Eebt2Q8nFQJ2dKa1FTE9X0J",
+				RequestID: "req_28w2Eebt2Q8nFQJ2dKa1FTE9X0J",
 			},
-			want: `{"PK":"ACCESS_REQUEST#","SK":"req_28w2Eebt2Q8nFQJ2dKa1FTE9X0J","GSI1PK":"ACCESS_REQUEST#user","GSI1SK":"req_28w2Eebt2Q8nFQJ2dKa1FTE9X0J","GSI2PK":"ACCESS_REQUEST#PENDING","GSI2SK":"user#req_28w2Eebt2Q8nFQJ2dKa1FTE9X0J","GSI3PK":"ACCESS_REQUEST#user","GSI3SK":"292277026596-12-04T15:30:07Z","GSI4PK":"ACCESS_REQUEST#user#rul_123","GSI4SK":"292277026596-12-04T15:30:07Z"}`,
+			want: `{"PK":"ACCESS_REQUEST_EVENT#","SK":"req_28w2Eebt2Q8nFQJ2dKa1FTE9X0J#his_28w2Eebt2Q8nFQJ2dKa1FTE9X0J"}`,
 		},
 	}
 
