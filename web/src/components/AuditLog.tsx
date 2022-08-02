6a1a51475406109fbeebe4dc707014cebfc9e166
meyerjrr <jrmeyer339@gmail.com>
2022-08-02 17:33:58 +1000

    clean up errors in the audit log

--- web/src/components/AuditLog.tsx
+++ web/src/components/AuditLog.tsx
@@ -1,6 +1,15 @@
 import { Box, VStack } from "@chakra-ui/layout";
-import { Skeleton, Text } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Skeleton,
+  Text,
+} from "@chakra-ui/react";
 import React, { useMemo } from "react";
+import { useUser } from "../utils/context/userContext";
 import {
   useGetUser,
   useListRequestEvents,
@@ -11,6 +20,8 @@ import { CFTimelineRow } from "./CFTimelineRow";
 export const AuditLog: React.FC<{ request?: RequestDetail }> = ({
   request,
 }) => {
+  const user = useUser();
+
   const { data } = useListRequestEvents(request?.id || "");
   const events = useMemo(() => {
     const items: JSX.Element[] = [];
@@ -47,12 +58,37 @@ export const AuditLog: React.FC<{ request?: RequestDetail }> = ({
           <CFTimelineRow
             arrLength={l}
             header={
-              <Text>
-                {`Grant status changed from ${e.fromGrantStatus?.toLowerCase()} to
-              ${e.toGrantStatus?.toLowerCase()} due to reason: ${
-                  e.grantFailureReason
-                }`}
-              </Text>
+              <>
+                <Text>Grant failed due to an error:</Text>
+                <Accordion allowToggle>
+                  <AccordionItem borderStyle="none">
+                    <h2>
+                      <AccordionButton>
+                        <Text
+                          fontSize="sm"
+                          color="#757575"
+                          fontWeight="normal"
+                          flex="1"
+                          textAlign="left"
+                        >
+                          Details
+                        </Text>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      <Text
+                        fontSize="sm"
+                        fontWeight="normal"
+                        flex="1"
+                        textAlign="left"
+                      >
+                        {e.grantFailureReason}
+                      </Text>
+                    </AccordionPanel>
+                  </AccordionItem>
+                </Accordion>
+              </>
             }
             index={i}
             body={new Date(e.createdAt).toString()}
