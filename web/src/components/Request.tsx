4fe7c9c17766deabc0624ef985034d01d39e0ba5
joshuawilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-08-12 10:46:30 +0800

    Add updated instruction to aws sso

--- web/src/components/Request.tsx
+++ web/src/components/Request.tsx
@@ -1,19 +1,22 @@
-import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import { CheckIcon, CopyIcon, DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import {
   Avatar,
   Badge,
   Box,
   Button,
   ButtonGroup,
+  Code,
   Flex,
   HStack,
   IconButton,
   Link,
   Skeleton,
   SkeletonText,
+  Spacer,
   Stack,
   Text,
   Tooltip,
+  useClipboard,
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
@@ -45,6 +48,7 @@ import { ProviderIcon } from "./icons/providerIcon";
 import EditRequestTimeModal from "./modals/EditRequestTimeModal";
 import RevokeConfirmationModal from "./modals/RevokeConfirmationModal";
 import { RequestStatusCell, StatusCell } from "./StatusCell";
+import { CFCodeSnippet } from "./CFCodeSnippet";
 
 interface RequestProps {
   request?: RequestDetail;
@@ -244,6 +248,53 @@ export const RequestAccessInstructions: React.FC = () => {
               {props.children}
             </Text>
           ),
+          code: (props) => {
+            console.log({ props });
+            const { children, node } = props;
+            let value = "";
+            if (node.children.length == 1 && node.children[0].type == "text") {
+              value = node.children[0].value;
+            }
+            console.log({ value });
+
+            const { hasCopied, onCopy } = useClipboard(value);
+            return (
+              <Stack>
+                <Code
+                  padding={0}
+                  bg="white"
+                  borderRadius="8px"
+                  borderColor="neutrals.300"
+                  borderWidth="1px"
+                >
+                  <Flex
+                    borderColor="neutrals.300"
+                    borderBottomWidth="1px"
+                    py="8px"
+                    px="16px"
+                    minH="36px"
+                  >
+                    <Spacer />
+                    <IconButton
+                      variant="ghost"
+                      h="20px"
+                      icon={hasCopied ? <CheckIcon /> : <CopyIcon />}
+                      onClick={onCopy}
+                      aria-label={"Copy"}
+                    />
+                  </Flex>
+                  <Text
+                    overflowX="auto"
+                    color="neutrals.700"
+                    padding={4}
+                    whiteSpace="pre"
+                  >
+                    {children}
+                  </Text>
+                </Code>
+              </Stack>
+            );
+          },
         }}
       >
         {data.instructions}
