1ce9915f17434643c1b467a7e59c7c1aed2cbf98
joshuawilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-08-12 09:22:04 +0800

    Add tag check to permission set list method

--- accesshandler/pkg/providers/aws/sso/options.go
+++ accesshandler/pkg/providers/aws/sso/options.go
@@ -20,26 +20,40 @@ func (p *Provider) Options(ctx context.Context, arg string) ([]types.Option, err
 		opts := []types.Option{}
 		hasMore := true
 		var nextToken *string
+
 		for hasMore {
 			o, err := p.client.ListPermissionSets(ctx, &ssoadmin.ListPermissionSetsInput{
 				InstanceArn: aws.String(p.instanceARN.Get()),
 				NextToken:   nextToken,
 			})
+
 			if err != nil {
 				return nil, err
 			}
 			nextToken = o.NextToken
 			hasMore = nextToken != nil
+
 			for _, arn := range o.PermissionSets {
+				// the user should exist in AWS SSO.
+				arnCopy := arn
+
 				po, err := p.client.DescribePermissionSet(ctx, &ssoadmin.DescribePermissionSetInput{
-					InstanceArn: aws.String(p.instanceARN.Get()), PermissionSetArn: aws.String(arn),
+					InstanceArn: aws.String(p.instanceARN.Get()), PermissionSetArn: aws.String(arnCopy),
 				})
 				if err != nil {
 					return nil, err
 				}
-				opts = append(opts, types.Option{Label: *po.PermissionSet.Name, Value: arn})
+				hasTag, err := p.checkPermissionSetIsTagged(ctx, arnCopy)
+				if err != nil {
+					return nil, err
+				}
+				if hasTag {
+					opts = append(opts, types.Option{Label: *po.PermissionSet.Name, Value: arnCopy})
+				}
+
 			}
 		}
+
 		return opts, nil
 	case "accountId":
 		log := zap.S().With("arg", arg)
@@ -65,4 +79,26 @@ func (p *Provider) Options(ctx context.Context, arg string) ([]types.Option, err
 
 	return nil, &providers.InvalidArgumentError{Arg: arg}
 
+}
+
+func (p *Provider) checkPermissionSetIsTagged(ctx context.Context, permissionSetARN string) (bool, error) {
+	hasMore := true
+	var nextToken *string
+	for hasMore {
+		tags, err := p.client.ListTagsForResource(ctx, &ssoadmin.ListTagsForResourceInput{
+			InstanceArn: aws.String(p.instanceARN.Get()),
+			ResourceArn: aws.String(permissionSetARN),
+		})
+		if err != nil {
+			return false, err
+		}
+		nextToken = tags.NextToken
+		hasMore = nextToken != nil
+		for _, tag := range tags.Tags {
+			if aws.ToString(tag.Key) == "commonfate.io/permission-set" {
+				return true, nil
+			}
+		}
+	}
+	return false, nil
 }
