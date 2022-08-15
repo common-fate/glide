f03852d2abb88046e70c02dbb981dde0fd3616a8
joshuawilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-08-15 09:05:08 +0800

    fix lint error

--- web/src/components/Request.tsx
+++ web/src/components/Request.tsx
@@ -48,6 +48,7 @@ import { ProviderIcon } from "./icons/providerIcon";
 import EditRequestTimeModal from "./modals/EditRequestTimeModal";
 import RevokeConfirmationModal from "./modals/RevokeConfirmationModal";
 import { RequestStatusCell, StatusCell } from "./StatusCell";
+import { CodeProps } from "react-markdown/lib/ast-to-react";
 
 interface RequestProps {
   request?: RequestDetail;
@@ -247,51 +248,7 @@ export const RequestAccessInstructions: React.FC = () => {
               {props.children}
             </Text>
           ),
-          code: (props) => {
-            const { children, node } = props;
-            let value = "";
-            if (node.children.length == 1 && node.children[0].type == "text") {
-              value = node.children[0].value;
-            }
-
-            const { hasCopied, onCopy } = useClipboard(value);
-            return (
-              <Stack>
-                <Code
-                  padding={0}
-                  bg="white"
-                  borderRadius="8px"
-                  borderColor="neutrals.300"
-                  borderWidth="1px"
-                >
-                  <Flex
-                    borderColor="neutrals.300"
-                    borderBottomWidth="1px"
-                    py="8px"
-                    px="16px"
-                    minH="36px"
-                  >
-                    <Spacer />
-                    <IconButton
-                      variant="ghost"
-                      h="20px"
-                      icon={hasCopied ? <CheckIcon /> : <CopyIcon />}
-                      onClick={onCopy}
-                      aria-label={"Copy"}
-                    />
-                  </Flex>
-                  <Text
-                    overflowX="auto"
-                    color="neutrals.700"
-                    padding={4}
-                    whiteSpace="pre-wrap"
-                  >
-                    {children}
-                  </Text>
-                </Code>
-              </Stack>
-            );
-          },
+          code: CodeInstruction,
         }}
       >
         {data.instructions}
@@ -300,6 +257,51 @@ export const RequestAccessInstructions: React.FC = () => {
   );
 };
 
+const CodeInstruction: React.FC<CodeProps> = (props) => {
+  const { children, node } = props;
+  let value = "";
+  if (node.children.length == 1 && node.children[0].type == "text") {
+    value = node.children[0].value;
+  }
+
+  const { hasCopied, onCopy } = useClipboard(value);
+  return (
+    <Stack>
+      <Code
+        padding={0}
+        bg="white"
+        borderRadius="8px"
+        borderColor="neutrals.300"
+        borderWidth="1px"
+      >
+        <Flex
+          borderColor="neutrals.300"
+          borderBottomWidth="1px"
+          py="8px"
+          px="16px"
+          minH="36px"
+        >
+          <Spacer />
+          <IconButton
+            variant="ghost"
+            h="20px"
+            icon={hasCopied ? <CheckIcon /> : <CopyIcon />}
+            onClick={onCopy}
+            aria-label={"Copy"}
+          />
+        </Flex>
+        <Text
+          overflowX="auto"
+          color="neutrals.700"
+          padding={4}
+          whiteSpace="pre-wrap"
+        >
+          {children}
+        </Text>
+      </Code>
+    </Stack>
+  );
+};
 export const RequestTime: React.FC = () => {
   const { request } = useContext(Context);
   const timing = request?.timing;
