51441a0211e62eda95888d070f732f562c725138
jordiup <jordi.hermoso.up@gmail.com>
2022-08-18 08:59:43 +0800

    Update request.go

--- pkg/api/request.go
+++ pkg/api/request.go
@@ -331,6 +331,8 @@ func (a *API) GetAccessInstructions(w http.ResponseWriter, r *http.Request, requ
 		apio.JSON(ctx, w, res.JSON404.Error, res.StatusCode())
 	case http.StatusBadRequest:
 		apio.JSON(ctx, w, res.JSON400.Error, res.StatusCode())
+	default:
+		apio.Error(ctx, w, fmt.Errorf("unexpected status code: %d", res.StatusCode()))
 	}
 
 }
