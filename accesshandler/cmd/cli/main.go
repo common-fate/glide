21aaf15cb13ad804587d77d47e3b8dec7de99d01
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-08-19 13:23:04 +0800

    remove test command

--- accesshandler/cmd/cli/main.go
+++ accesshandler/cmd/cli/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/common-fate/granted-approvals/accesshandler/cmd/cli/commands/grants"
-	eksrolessso "github.com/common-fate/granted-approvals/accesshandler/pkg/providers/aws/eks-roles-sso"
-	"github.com/common-fate/granted-approvals/pkg/gconfig"
-	"github.com/common-fate/granted-approvals/pkg/types"
 	"github.com/urfave/cli/v2"
 	"go.uber.org/zap"
 )
@@ -20,45 +15,12 @@ func main() {
 	}
 
 	app := &cli.App{
-		Flags:       flags,
-		Name:        "granted",
-		Usage:       "https://granted.dev",
-		UsageText:   "granted [global options] command [command options] [arguments...]",
-		HideVersion: false,
-		Commands: []*cli.Command{&grants.Command, {Name: "test", Action: func(c *cli.Context) error {
-			ctx := c.Context
-			var p eksrolessso.Provider
-			cfg := p.Config()
-			err := cfg.Load(ctx, gconfig.JSONLoader{Data: []byte(`{"clusterName":"provider-eks-test","clusterRegion":"ap-southeast-2","identityStoreId":"d-976708da7d","instanceArn":"arn:aws:sso:::instance/ssoins-825968feece9a0b6","namespace":"default","ssoRegion":"ap-southeast-2"}`)})
-			if err != nil {
-				return err
-			}
-
-			a := eksrolessso.Args{
-				Role: "pod-reader",
-			}
-			b, err := json.Marshal(a)
-			if err != nil {
-				return err
-			}
-			err = p.Init(ctx)
-			if err != nil {
-				return err
-			}
-			grantid := types.NewRequestID()
-			err = p.Grant(ctx, "jack@commonfate.io", b, grantid)
-			if err != nil {
-				return err
-			}
-			time.Sleep(time.Minute * 1)
-
-			err = p.Revoke(ctx, "jack@commonfate.io", b, grantid)
-			if err != nil {
-				return err
-			}
-
-			return nil
-		}}},
+		Flags:                flags,
+		Name:                 "granted",
+		Usage:                "https://granted.dev",
+		UsageText:            "granted [global options] command [command options] [arguments...]",
+		HideVersion:          false,
+		Commands:             []*cli.Command{&grants.Command},
 		EnableBashCompletion: true,
 	}
 
