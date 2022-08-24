d7a965d89ded42a740c2d5f84519a76e87f2d91a
Manish Chaulagain <eddie@mac.local>
2022-08-24 16:24:51 +1000

    Update description and usage for identity cmd

--- cmd/gdeploy/commands/identity/identity.go
+++ cmd/gdeploy/commands/identity/identity.go
@@ -8,11 +8,10 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-// TODO: Update description and usage text.
 var Command = cli.Command{
 	Name:        "identity",
-	Description: "configure identity",
-	Usage:       "Add an identity provider",
+	Description: "identity commands are used to manage how your users login to Granted Approvals, manage your Cognito user pool, or configure your corporate SSO provider",
+	Usage:       "Configure how your users login to Granted Approvals",
 	Action:      cli.ShowSubcommandHelp,
 	Subcommands: []*cli.Command{&sso.SSOCommand, &users.UsersCommand, &groups.GroupsCommand, &sync.SyncCommand},
 }
