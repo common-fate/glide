c0944dc35fa2270b3f3bd376d96ff49073e71db5
jordiup <jordi.hermoso.up@gmail.com>
2022-09-02 14:44:11 +0800

    More docs cleanup

--- web/src/components/CodeInstruction.tsx
+++ web/src/components/CodeInstruction.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { CheckIcon, CopyIcon } from "@chakra-ui/icons";
-import { CodeProps } from "react-markdown/lib/ast-to-react";
 import {
   useClipboard,
   Stack,
