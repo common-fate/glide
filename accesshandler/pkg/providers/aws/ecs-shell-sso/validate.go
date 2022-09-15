354e735d6ad76760c09991cd1873d707de4bb848
meyerjrr <jrmeyer339@gmail.com>
2022-09-15 16:41:24 +1000

    put back validate code for ecs

--- accesshandler/pkg/providers/aws/ecs-shell-sso/validate.go
+++ accesshandler/pkg/providers/aws/ecs-shell-sso/validate.go
@@ -8,10 +8,12 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
 	"github.com/aws/aws-sdk-go-v2/service/identitystore"
+	"github.com/aws/aws-sdk-go-v2/service/identitystore/types"
 	"github.com/aws/aws-sdk-go-v2/service/organizations"
 	orgtypes "github.com/aws/aws-sdk-go-v2/service/organizations/types"
 	"github.com/common-fate/granted-approvals/accesshandler/pkg/diagnostics"
 	"github.com/common-fate/granted-approvals/accesshandler/pkg/providers"
+	"golang.org/x/sync/errgroup"
 )
 
 func (p *Provider) ensureAccountExists(ctx context.Context, accountID string) error {
@@ -29,45 +31,44 @@ func (p *Provider) ensureAccountExists(ctx context.Context, accountID string) er
 // Validate the access against AWS SSO without actually granting it.
 // This provider requires that the user name matches the user's email address.
 func (p *Provider) Validate(ctx context.Context, subject string, args []byte) error {
-	return fmt.Errorf("test error this should show in the frontend")
 
 	// run the validations concurrently, as we need to wait for the API to respond.
-	// g := new(errgroup.Group)
+	g := new(errgroup.Group)
 
-	// // the user should exist in AWS SSO.
-	// g.Go(func() error {
-	// 	res, err := p.idStoreClient.ListUsers(ctx, &identitystore.ListUsersInput{
-	// 		IdentityStoreId: aws.String(p.identityStoreID.Get()),
-	// 		Filters: []types.Filter{{
-	// 			AttributePath:  aws.String("UserName"),
-	// 			AttributeValue: aws.String(subject),
-	// 		}},
-	// 	})
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	if len(res.Users) == 0 {
-	// 		return fmt.Errorf("could not find user %s in AWS SSO", subject)
-	// 	}
-	// 	if len(res.Users) > 1 {
-	// 		// this should never happen, but check it anyway.
-	// 		return fmt.Errorf("expected 1 user but found %v", len(res.Users))
-	// 	}
-	// 	return nil
-	// })
+	// the user should exist in AWS SSO.
+	g.Go(func() error {
+		res, err := p.idStoreClient.ListUsers(ctx, &identitystore.ListUsersInput{
+			IdentityStoreId: aws.String(p.identityStoreID.Get()),
+			Filters: []types.Filter{{
+				AttributePath:  aws.String("UserName"),
+				AttributeValue: aws.String(subject),
+			}},
+		})
+		if err != nil {
+			return err
+		}
+		if len(res.Users) == 0 {
+			return fmt.Errorf("could not find user %s in AWS SSO", subject)
+		}
+		if len(res.Users) > 1 {
+			// this should never happen, but check it anyway.
+			return fmt.Errorf("expected 1 user but found %v", len(res.Users))
+		}
+		return nil
+	})
 
-	// // the account should exist.
-	// g.Go(func() error {
-	// 	return p.ensureAccountExists(ctx, p.awsAccountID)
-	// })
+	// the account should exist.
+	g.Go(func() error {
+		return p.ensureAccountExists(ctx, p.awsAccountID)
+	})
 
-	// //ensure that the ecs cluster exists
-	// _, err := p.ecsClient.DescribeClusters(ctx, &ecs.DescribeClustersInput{Clusters: []string{p.ecsClusterARN.Get()}})
-	// if err != nil {
-	// 	return err
-	// }
+	//ensure that the ecs cluster exists
+	_, err := p.ecsClient.DescribeClusters(ctx, &ecs.DescribeClustersInput{Clusters: []string{p.ecsClusterARN.Get()}})
+	if err != nil {
+		return err
+	}
 
-	// return g.Wait()
+	return g.Wait()
 }
 
 func (p *Provider) ValidateConfig() map[string]providers.ConfigValidationStep {
