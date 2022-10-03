ce3bdc11046ebea8401fb355e7d611d0ce8e6967
Manish Chaulagain <manish23chaulagain@gmail.com>
2022-10-04 06:57:42 +1100

    CF-145 Add `select all` options to MultiSelect that selects all option (#284)
    * CF-145 Add select-all option that selects all option
    
    * CF-145 Prettify
    
    * Apply suggestions from code review
    
    Co-authored-by: Manish Chaulagain <manish23chaulagain@gmail.com>
    
    Co-authored-by: Manish Chaulagain <eddie@MacBook-Pro-3.local>
    Co-authored-by: Chris Norman <17420369+chrnorm@users.noreply.github.com>

--- web/src/components/forms/access-rule/steps/Provider.tsx
+++ web/src/components/forms/access-rule/steps/Provider.tsx
@@ -213,6 +213,7 @@ const WithField: React.FC<FieldProps> = (props) => {
             rules={{ required: true, minLength: 1 }}
             fieldName={`target.with.${props.name}`}
             options={data.options}
+            shouldAddSelectAllOption={true}
           />
 
           <RefreshButton providerId={providerId} argId={props.name} />
