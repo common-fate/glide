c2b5fb433eb391773bc54d03a78d3f2479fe9916
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-10-17 18:07:09 +0800

    improve performance of graph using go routines

--- accesshandler/pkg/providers/aws/sso-v2/organization-graph-2.go
+++ accesshandler/pkg/providers/aws/sso-v2/organization-graph-2.go
@@ -2,15 +2,25 @@ package ssov2
 
 import (
 	"context"
+	"fmt"
+	"sync"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/organizations"
 	organizationTypes "github.com/aws/aws-sdk-go-v2/service/organizations/types"
+	"golang.org/x/sync/errgroup"
 )
 
+type OrganizationGraph struct {
+	Root    *Node
+	idMap   map[string]*Node
+	idMapMu sync.Mutex
+}
 type Node struct {
-	ID     string
-	Graph  *OrganizationGraph
-	Parent *Node
+	descendantsMu sync.Mutex
+	ID            string
+	Graph         *OrganizationGraph
+	Parent        *Node
 	// Direct children of this node
 	Children []*Node
 	// All descendants of this node
@@ -19,25 +29,27 @@ type Node struct {
 	Account            *organizationTypes.Account
 	Root               *organizationTypes.Root
 }
-type OrganizationGraph struct {
-	Root  *Node
-	idMap map[string]*Node
-}
 
 func (p *Provider) buildOrganizationGraph(ctx context.Context) (*OrganizationGraph, error) {
 	roots, err := p.orgClient.ListRoots(ctx, &organizations.ListRootsInput{})
 	if err != nil {
 		return nil, err
 	}
+
+	if len(roots.Roots) != 1 {
+		return nil, fmt.Errorf("expected to find 1 organization root but found %v", len(roots.Roots))
+	}
+	root := roots.Roots[0]
 	graph := OrganizationGraph{
 		Root: &Node{
-			ID:   *roots.Roots[0].Id,
-			Root: &roots.Roots[0],
+			ID:   aws.ToString(root.Id),
+			Root: &root,
 		},
 	}
+
 	graph.Root.Graph = &graph
 
-	graph.idMap = map[string]*Node{*roots.Roots[0].Id: graph.Root}
+	graph.idMap = map[string]*Node{aws.ToString(root.Id): graph.Root}
 	err = graph.Root.BuildGraph(ctx, p)
 	if err != nil {
 		return nil, err
@@ -59,7 +71,7 @@ func (n *Node) DescendantAccountIDs() []string {
 	var accountIDs []string
 	for _, child := range n.Descendants {
 		if child.IsAccount() {
-			accountIDs = append(accountIDs, *child.Account.Id)
+			accountIDs = append(accountIDs, aws.ToString(child.Account.Id))
 		}
 	}
 	return accountIDs
@@ -68,7 +80,7 @@ func (n *Node) DescendantOrganizationalUnitIDs() []string {
 	var accountIDs []string
 	for _, child := range n.Descendants {
 		if child.IsOrganizationalUnit() {
-			accountIDs = append(accountIDs, *child.OrganizationalUnit.Id)
+			accountIDs = append(accountIDs, aws.ToString(child.OrganizationalUnit.Id))
 		}
 	}
 	return accountIDs
@@ -103,45 +115,59 @@ func (n *Node) DescendantOrganizationalUnits() []*Node {
 
 func (n *Node) BuildGraph(ctx context.Context, provider *Provider) error {
 	if n.IsOrganizationalUnit() || n.IsRoot() {
+		g, gctx := errgroup.WithContext(ctx)
 		childOUs, err := provider.listChildOusForParent(ctx, n.ID)
 		if err != nil {
 			return err
 		}
 		for i := range childOUs {
 			node := &Node{
-				ID:                 *childOUs[i].Id,
+				ID:                 aws.ToString(childOUs[i].Id),
 				OrganizationalUnit: &childOUs[i],
 				Parent:             n,
 				Graph:              n.Graph,
 			}
+
 			n.Children = append(n.Children, node)
+			n.Graph.idMapMu.Lock()
 			n.Graph.idMap[node.ID] = node
+			n.Graph.idMapMu.Unlock()
+			g.Go(func() error {
+				return node.BuildGraph(gctx, provider)
+			})
+
 		}
-		childAccounts, err := provider.listChildAccountsForParent(ctx, n.ID)
-		if err != nil {
-			return err
-		}
-		for i := range childAccounts {
-			node := &Node{
-				ID:      *childAccounts[i].Id,
-				Account: &childAccounts[i],
-				Parent:  n,
-				Graph:   n.Graph,
-			}
-			n.Children = append(n.Children, node)
-			n.Graph.idMap[node.ID] = node
-		}
-		// assign all children as decendants
-		n.Descendants = n.Children
-		for i := range n.Children {
-			err = n.Children[i].BuildGraph(ctx, provider)
+		g.Go(func() error {
+			childAccounts, err := provider.listChildAccountsForParent(ctx, n.ID)
 			if err != nil {
 				return err
 			}
+			for i := range childAccounts {
+				node := &Node{
+					ID:      aws.ToString(childAccounts[i].Id),
+					Account: &childAccounts[i],
+					Parent:  n,
+					Graph:   n.Graph,
+				}
+				n.Children = append(n.Children, node)
+				n.Graph.idMapMu.Lock()
+				n.Graph.idMap[node.ID] = node
+				n.Graph.idMapMu.Unlock()
+			}
+			return nil
+		})
+
+		err = g.Wait()
+		if err != nil {
+			return err
 		}
+		// assign all children as decendants
+		n.Descendants = append(n.Descendants, n.Children...)
 		if n.Parent != nil {
 			// append decendants to parent
+			n.Parent.descendantsMu.Lock()
 			n.Parent.Descendants = append(n.Parent.Descendants, n.Descendants...)
+			n.Parent.descendantsMu.Unlock()
 		}
 
 	}
