5cf78be67b2d32a013c9193a37d453ec7f131dbc
jordiup <jordi.hermoso.up@gmail.com>
2022-10-20 18:58:28 +0800

    Preview II
    * Initial working

--- web/src/components/forms/access-rule/components/ProviderPreview.tsx
+++ web/src/components/forms/access-rule/components/ProviderPreview.tsx
@@ -17,26 +17,34 @@ import {
   useGetProviderArgs,
   useListProviderArgOptions,
 } from "../../../../utils/backend-client/admin/admin";
+import {
+  Argument,
+  ArgumentFormElement,
+  GroupOption,
+  Option,
+} from "../../../../utils/backend-client/types/accesshandler-openapi.yml";
 import { CopyableOption } from "../../../CopyableOption";
 import { DynamicOption } from "../../../DynamicOption";
 import { BoltIcon } from "../../../icons/Icons";
 import { ProviderIcon } from "../../../icons/providerIcon";
 import { AccessRuleFormData } from "../CreateForm";
 
-export const ProviderPreview: React.FC = () => {
-  const { watch } = useFormContext<AccessRuleFormData>();
-  const target = watch("target");
-  const { data } = useGetProviderArgs(target?.providerId || "");
-  const { data: provider } = useGetProvider(target?.providerId);
+interface ProviderArgumentFieldProps {
+  argument: Argument;
+  providerId: string;
+}
 
-  if (
-    target?.providerId === undefined ||
-    target?.providerId === "" ||
-    data === undefined ||
-    provider === undefined
-  ) {
-    return null;
-  }
+export const ProviderPreview: React.FC<ProviderArgumentFieldProps> = ({
+  argument,
+  providerId,
+}) => {
+  const methods = useFormContext<AccessRuleFormData>();
+  const target = methods.watch("target");
+
+  const { data: provider } = useGetProvider(target.providerId);
+  const { data: providerArgs } = useGetProviderArgs(target.providerId ?? "");
+
+  if (!provider) return null;
 
   return (
     <VStack w="100%" align="flex-start">
@@ -44,120 +52,245 @@ export const ProviderPreview: React.FC = () => {
         <ProviderIcon shortType={provider.type} />
         <Text>{provider.id}</Text>
       </HStack>
-      <VStack w="100%" align={"flex-start"} spacing={4}>
-        {data &&
-          target.multiSelects &&
-          Object.entries(target.multiSelects).map(([k, v]) => {
-            const arg = data[k];
+      {providerArgs &&
+        target?.providerId &&
+        Object.values(providerArgs).map((v) => (
+          <_ProviderPreview argument={v} providerId={target?.providerId} />
+        ))}
+    </VStack>
+  );
+};
 
-            // This will now fetch all arg options i.e.
-            // { label: 'AWSReadOnlyAccess', value: 'arn:aws...' }
-            // This can make our flat values copyable
-            const { data: argOptions } = useListProviderArgOptions(
-              provider.id,
-              k
-            );
-            if (v.length === 0) return null;
+export const _ProviderPreview: React.FC<ProviderArgumentFieldProps> = ({
+  argument,
+  providerId,
+}) => {
+  const { formState, watch } = useFormContext<AccessRuleFormData>();
+  // const target = watch("target");
+  // const target = watch("target");
+  // const { data } = useGetProviderArgs(target?.providerId || "");
+  const { data: _provider } = useGetProvider(providerId);
 
-            return (
-              <VStack
-                w="100%"
-                align={"flex-start"}
-                spacing={4}
-                key={k}
-                p={4}
-                rounded="md"
-                border="1px solid"
-                borderColor="gray.300"
-              >
-                <Box>
-                  <Text textStyle={"Body/Medium"} color="neutrals.500">
-                    {arg.title}s
-                  </Text>
-                  {/* {arg.description && (
+  // if (
+  //   target?.providerId === undefined ||
+  //   target?.providerId === "" ||
+  //   data === undefined ||
+  //   _provider === undefined
+  // ) {
+  //   return null;
+  // }
+
+  const { data: argOptions } = useListProviderArgOptions(
+    providerId,
+    argument.id
+  );
+  const multiSelectsError = formState.errors.target?.multiSelects;
+
+  const [argumentGroups, multiSelects] = watch([
+    `target.argumentGroups.${argument.id}`,
+    `target.multiSelects.${argument.id}`,
+  ]);
+  /** get all the group children (for aws these are the accounts for the OUs) */
+  const effectiveViaGroups =
+    (argumentGroups &&
+      Object.entries(argumentGroups || {}).flatMap(
+        ([groupId, selectedGroupValues]) => {
+          // get all the accounts for the selected group value
+          const group = argOptions?.groups ? argOptions?.groups[groupId] : [];
+          return selectedGroupValues.flatMap((groupValue) => {
+            return group.find((g) => g.value === groupValue)?.children || [];
+          });
+        }
+      )) ??
+    [];
+
+  type Obj = {
+    option: Option;
+    parentGroup?: GroupOption;
+  };
+
+  // Desired output: an array of Options (value and label) with ID(s) that can lookup into the richer map object
+  // Step 1: get all the options from the groups
+  // Step 2: get all the options from the multi-selects
+  // Step 3: store the options in an array of type Obj
+  // Step 4: remove any duplicate Option.key Option.value paires
+
+  const effectiveGroups =
+    argumentGroups &&
+    Object.entries(argumentGroups || {}).flatMap(
+      ([groupId, selectedGroupValues]) => {
+        // get all the accounts for the selected group value
+        const group = argOptions?.groups ? argOptions?.groups?.[groupId] : [];
+        return (
+          selectedGroupValues
+            .flatMap((groupValue) => {
+              return group.find((g) => g.value === groupValue) ?? null;
+            })
+            // Now remove any null values
+            .filter((g) => g)
+        );
+      }
+    );
+
+  const res: Obj[] = [];
+
+  effectiveGroups?.forEach((g) => {
+    g?.children?.forEach((c) => {
+      const option = argOptions?.options?.find((o) => o.value === c);
+      option && res.push({ option, parentGroup: g });
+    });
+  });
+  multiSelects?.forEach((ms) => {
+    const option = argOptions?.options?.find((o) => o.value === ms);
+    option && res.push({ option });
+  });
+
+  // Now remove any duplicate Option.label Option.value paires
+  // With a preference for the parentGroup option
+  const uniqueRes = res.reduce((acc, cur) => {
+    const existing = acc.find(
+      (a) =>
+        a.option.value === cur.option.value &&
+        a.option.label === cur.option.label
+    );
+    if (existing) {
+      if (cur.parentGroup) {
+        acc.splice(acc.indexOf(existing), 1, cur);
+      }
+    } else {
+      acc.push(cur);
+    }
+    return acc;
+  }, [] as Obj[]);
+
+  // effectiveViaGroups.filter(g => {
+  // })
+
+  // Filter to remove duplicates
+  const effectiveAccountIds = [
+    ...(multiSelects || []),
+    ...effectiveViaGroups,
+  ].filter((v, i, a) => a.indexOf(v) === i);
+
+  // DE = we want to associate each 'effectiveOption' with either a group (dynamic) or a multiSelect (single)
+  // We can do this by checking if the effectiveOption is in the multiSelects array
+  // Or, avoid casting it to a string to begin with
+  const effectiveOptions =
+    argOptions?.options.filter((option) => {
+      return effectiveAccountIds.includes(option.value);
+    }) || [];
+  const required = effectiveOptions.length === 0;
+
+  if (!_provider) return null;
+
+  return (
+    <VStack
+      w="100%"
+      align={"flex-start"}
+      spacing={4}
+      // key={k}
+      p={4}
+      rounded="md"
+      border="1px solid"
+      borderColor="gray.300"
+    >
+      <Box>
+        <Text textStyle={"Body/Medium"} color="neutrals.500">
+          {argument.title}s
+        </Text>
+        {/* {arg.description && (
                     <Text textStyle={"Body/Medium"} color="neutrals.500">
                       {arg.description}
                     </Text>
                   )} */}
-                  <Wrap>
-                    {v?.map((opt) => {
-                      return (
-                        <DynamicOption
-                          key={"cp-" + opt}
-                          label={
-                            argOptions?.options?.find((d) => d.value === opt)
-                              ?.label ?? ""
-                          }
-                          value={opt}
-                        />
-                      );
-                    })}
-                  </Wrap>
-                </Box>
-                {target.argumentGroups &&
-                  target.argumentGroups[k] &&
-                  arg.groups &&
-                  Object.entries(target.argumentGroups[k]).map(
-                    ([groupId, groupValues]) => {
-                      if (!arg.groups || groupValues.length === 0) {
-                        return null;
-                      }
-                      const group = arg.groups[groupId];
-                      return (
-                        <Box>
-                          <Flex>
-                            <Text
-                              textStyle={"Body/Medium"}
-                              color="neutrals.500"
-                            >
-                              {group.title}s
-                            </Text>
-                            <Tooltip label="Dynamic Field" hasArrow={true}>
-                              <Circle
-                                display="inline-flex"
-                                size="24px"
-                                px={1}
-                                rounded="full"
-                              >
-                                <BoltIcon boxSize="12px" color="neutrals.400" />
-                              </Circle>
-                            </Tooltip>
-                          </Flex>
-                          {/* {group.description && (
-                            <Text
-                              textStyle={"Body/Medium"}
-                              color="neutrals.500"
-                            >
-                              {group.description}
-                            </Text>
-                          )} */}
-                          <Wrap>
-                            {groupValues.map((groupValue) => {
-                              if (!argOptions?.groups) {
-                                return null;
-                              }
-                              const groupOptions = argOptions.groups[groupId];
-                              return (
-                                <DynamicOption
-                                  key={"cp-" + groupValue}
-                                  label={
-                                    groupOptions.find(
-                                      (d) => d.value === groupValue
-                                    )?.label ?? ""
-                                  }
-                                  value={groupValue}
-                                />
-                              );
-                            })}
-                          </Wrap>
-                        </Box>
-                      );
-                    }
-                  )}
-              </VStack>
+        <Wrap>
+          {multiSelects?.map((opt) => {
+            return (
+              <DynamicOption
+                key={"cp-" + opt}
+                label={
+                  argOptions?.options?.find((d) => d.value === opt)?.label ?? ""
+                }
+                value={opt}
+              />
             );
           })}
-      </VStack>
+        </Wrap>
+      </Box>
+      {argument.groups &&
+        Object.values(argument.groups).map((group) => {
+          // if (!argument.groups || group..length === 0) {
+          //   return null;
+          // }
+          return (
+            <Box>
+              <Flex>
+                <Text textStyle={"Body/Medium"} color="neutrals.500">
+                  {group.title}s
+                </Text>
+                <Tooltip label="Dynamic Field" hasArrow={true}>
+                  <Circle
+                    display="inline-flex"
+                    size="24px"
+                    px={1}
+                    rounded="full"
+                  >
+                    <BoltIcon boxSize="12px" color="neutrals.400" />
+                  </Circle>
+                </Tooltip>
+              </Flex>
+              {/* {group.description && (
+                    <Text
+                      textStyle={"Body/Medium"}
+                      color="neutrals.500"
+                    >
+                      {group.description}
+                    </Text>
+                  )} */}
+
+              {argumentGroups &&
+                Object.entries(argumentGroups ?? {}).length > 0 && (
+                  <Box>
+                    <Wrap>
+                      {Object.entries(argumentGroups).map(([k, v]) => {
+                        // const c =
+                        // the option is argOptions.groups[group.id
+                        const c = argOptions?.groups?.[k];
+                        // console.log(k, v, argOptions?.groups, c);
+
+                        if (!c) return null;
+
+                        return c.map((g) => (
+                          <DynamicOption
+                            label={g.label}
+                            value={g.value}
+                            parentGroup={g.parentGroup}
+                          />
+                        ));
+                      })}
+                    </Wrap>
+                  </Box>
+                )}
+            </Box>
+          );
+        })}
+      {argOptions?.groups &&
+        Object.entries(argOptions?.groups ?? {}).length > 0 && (
+          <Box>
+            <Wrap>
+              {uniqueRes &&
+                uniqueRes.map((c) => {
+                  return (
+                    <DynamicOption
+                      label={c.option.label}
+                      value={c.option.value}
+                      parentGroup={c.parentGroup}
+                    />
+                  );
+                })}
+            </Wrap>
+          </Box>
+        )}
     </VStack>
   );
 };
