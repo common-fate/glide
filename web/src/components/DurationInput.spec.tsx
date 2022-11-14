1e5d64cac4a8a24024d7aa266596e903cec3139a
jordiup <jordi.hermoso.up@gmail.com>
2022-11-14 15:48:54 +0800

    Update DurationInput.spec.tsx

--- web/src/components/DurationInput.spec.tsx
+++ web/src/components/DurationInput.spec.tsx
@@ -1,6 +1,6 @@
+import { expect, test } from "@playwright/experimental-ct-react";
 import * as React from "react";
 import { Days, DurationInput, Hours, Minutes, Weeks } from "./DurationInput";
-import { test, expect } from "@playwright/experimental-ct-react";
 
 test("minutes renders", async ({ mount }) => {
   const component = await mount(
@@ -97,4 +97,102 @@ test("weeks increment decrement", async ({ mount }) => {
   await component.locator(`#decrement >> visible=true`).first().click();
   await expect(component.locator("#week-duration-input")).toHaveValue("0");
   await expect(component.locator("#decrement")).toBeDisabled();
+});
+
+const MINUTE = 60;
+const HOUR = 3600;
+const DAY = 86400;
+const WEEK = 7 * DAY;
+const MONTH = 30 * DAY;
+
+const time = 3 * WEEK + 3 * DAY + 2 * HOUR + 1 * MINUTE; // 2080860
+
+test("duration with max of 3weeks, 3days, 2hours, 1min works as expected", async ({
+  mount,
+}) => {
+  const component = await mount(
+    <DurationInput
+      min={60}
+      max={time}
+      defaultValue={time}
+      value={time}
+      onChange={(v) => {}}
+    >
+      <Weeks />
+      <Days />
+      <Hours />
+      <Minutes />
+    </DurationInput>
+  );
+  await expect(component.locator("#week-duration-input")).toHaveValue("3");
+  await expect(component.locator("#day-duration-input")).toHaveValue("3");
+  await expect(component.locator("#hour-duration-input")).toHaveValue("2");
+  await expect(component.locator("#minute-duration-input")).toHaveValue("1");
+});
+
+test("actions work as expected", async ({ mount }) => {
+  const component = await mount(
+    <DurationInput
+      min={60}
+      max={time}
+      defaultValue={time}
+      value={time}
+      onChange={(v) => {}}
+    >
+      <Weeks />
+      <Days />
+      <Hours />
+      <Minutes />
+    </DurationInput>
+  );
+  // DECREMENT
+  // decrement week
+  await component.locator(`#decrement >> visible=true`).first().click();
+  await expect(component.locator("#week-duration-input")).toHaveValue("2");
+  await expect(component.locator("#day-duration-input")).toHaveValue("3");
+  await expect(component.locator("#hour-duration-input")).toHaveValue("2");
+  await expect(component.locator("#minute-duration-input")).toHaveValue("1");
+  // decrement day
+  await component.locator(`#decrement >> visible=true`).nth(1).click();
+  await expect(component.locator("#week-duration-input")).toHaveValue("2");
+  await expect(component.locator("#day-duration-input")).toHaveValue("2");
+  await expect(component.locator("#hour-duration-input")).toHaveValue("2");
+  await expect(component.locator("#minute-duration-input")).toHaveValue("1");
+  // decrement hour
+  await component.locator(`#decrement >> visible=true`).nth(2).click();
+  await expect(component.locator("#week-duration-input")).toHaveValue("2");
+  await expect(component.locator("#day-duration-input")).toHaveValue("2");
+  await expect(component.locator("#hour-duration-input")).toHaveValue("1");
+  await expect(component.locator("#minute-duration-input")).toHaveValue("1");
+  // decrement minute
+  await component.locator(`#decrement >> visible=true`).nth(3).click();
+  await expect(component.locator("#week-duration-input")).toHaveValue("2");
+  await expect(component.locator("#day-duration-input")).toHaveValue("2");
+  await expect(component.locator("#hour-duration-input")).toHaveValue("1");
+  await expect(component.locator("#minute-duration-input")).toHaveValue("0");
+  // INCREMENT
+  // increment week
+  await component.locator(`#increment >> visible=true`).first().click();
+  await expect(component.locator("#week-duration-input")).toHaveValue("3");
+  await expect(component.locator("#day-duration-input")).toHaveValue("2");
+  await expect(component.locator("#hour-duration-input")).toHaveValue("1");
+  await expect(component.locator("#minute-duration-input")).toHaveValue("0");
+  // increment day
+  await component.locator(`#increment >> visible=true`).nth(1).click();
+  await expect(component.locator("#week-duration-input")).toHaveValue("3");
+  await expect(component.locator("#day-duration-input")).toHaveValue("3");
+  await expect(component.locator("#hour-duration-input")).toHaveValue("1");
+  await expect(component.locator("#minute-duration-input")).toHaveValue("0");
+  // increment hour
+  await component.locator(`#increment >> visible=true`).nth(2).click();
+  await expect(component.locator("#week-duration-input")).toHaveValue("3");
+  await expect(component.locator("#day-duration-input")).toHaveValue("3");
+  await expect(component.locator("#hour-duration-input")).toHaveValue("2");
+  await expect(component.locator("#minute-duration-input")).toHaveValue("0");
+  // increment minute
+  await component.locator(`#increment >> visible=true`).nth(3).click();
+  await expect(component.locator("#week-duration-input")).toHaveValue("3");
+  await expect(component.locator("#day-duration-input")).toHaveValue("3");
+  await expect(component.locator("#hour-duration-input")).toHaveValue("2");
+  await expect(component.locator("#minute-duration-input")).toHaveValue("1");
 });
