2e12861e243eb690e2791f765454a212fdfa40f8
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-11-18 14:28:47 +0800

    okta test working

---
+++ accesshandler/pkg/providers/okta/okta_test.go
@@ -0,0 +1,59 @@
+package okta
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/common-fate/granted-approvals/accesshandler/pkg/providers/okta/fixtures"
+	"github.com/common-fate/granted-approvals/accesshandler/pkg/providertest"
+	"github.com/common-fate/granted-approvals/accesshandler/pkg/providertest/integration"
+	"github.com/hashicorp/go-multierror"
+	"github.com/joho/godotenv"
+)
+
+func TestIntegration(t *testing.T) {
+	ctx := context.Background()
+	_ = godotenv.Load("../../../../.env")
+	if os.Getenv("GRANTED_INTEGRATION_TEST") == "" {
+		t.Skip("GRANTED_INTEGRATION_TEST is not set, skipping integration testing")
+	}
+	var f fixtures.Fixtures
+	err := providertest.LoadFixture(ctx, "okta", &f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testcases := []integration.TestCase{
+		{
+			Name:              "ok",
+			Subject:           f.User,
+			Args:              fmt.Sprintf(`{"groupId": "%s"}`, f.GroupID),
+			WantValidationErr: nil,
+		},
+		{
+			Name:              "group not exist",
+			Subject:           f.User,
+			Args:              `{"groupId": "non-existent"}`,
+			WantValidationErr: &multierror.Error{Errors: []error{&GroupNotFoundError{Group: "non-existent"}}},
+		},
+		{
+			Name:              "subject not exist",
+			Subject:           "other",
+			Args:              fmt.Sprintf(`{"groupId": "%s"}`, f.GroupID),
+			WantValidationErr: &multierror.Error{Errors: []error{&UserNotFoundError{User: "other"}}},
+		},
+		{
+			Name:              "group and subject not exist",
+			Subject:           "other",
+			Args:              `{"groupId": "non-existent"}`,
+			WantValidationErr: &multierror.Error{Errors: []error{&UserNotFoundError{User: "other"}, &GroupNotFoundError{Group: "non-existent"}}},
+		},
+	}
+	w, err := integration.ProviderWith("okta")
+	if err != nil {
+		t.Fatal(err)
+	}
+	integration.RunTests(t, ctx, "okta", &Provider{}, testcases, integration.WithProviderConfig(w))
+}
