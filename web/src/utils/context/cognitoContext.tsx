e6094a4aa936ec152d2e236e8e45e9b7aa6edfe3
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-11-24 12:51:39 +0800

    prettier

--- web/src/utils/context/cognitoContext.tsx
+++ web/src/utils/context/cognitoContext.tsx
@@ -32,10 +32,8 @@ const CognitoProvider: React.FC<Props> = ({ children }) => {
   const [amplifyInitialising, setAmplifyInitializing] = useState(true);
   const [loadingCurrentUser, setLoadingCurrentUser] = useState(true);
   const [loggingOut, setLoggingOut] = useState(false);
-  const [
-    cognitoAuthenticatedUserEmail,
-    setCognitoAuthenticatedUserEmail,
-  ] = useState<string>();
+  const [cognitoAuthenticatedUserEmail, setCognitoAuthenticatedUserEmail] =
+    useState<string>();
   const loading = amplifyInitialising || loadingCurrentUser;
   const navigate = useNavigate();
   const search = useSearch<MyLocationGenerics>();
