0d8baacbeb77efbeb0613afd961674cebf687fde
Manish Chaulagain <eddie@MacBook-Pro-5.local>
2022-12-02 09:48:56 +1100

    Prettify

--- web/src/pages/access/request/[id].tsx
+++ web/src/pages/access/request/[id].tsx
@@ -173,16 +173,14 @@ const WithAccessRequestForm = () => {
   const navigate = useNavigate();
 
   const HTTP_STATUS_CODE_FORBIDDEN = 403;
-  if (
-    (error && error?.response?.status === HTTP_STATUS_CODE_FORBIDDEN)
-    ) {
-      return <ErrorNoAccess />
-    }
-    
+  if (error && error?.response?.status === HTTP_STATUS_CODE_FORBIDDEN) {
+    return <ErrorNoAccess />;
+  }
+
   // Users who are approval to the access rule but are not part of request group
   // should not be able to view the request form
-  if (rule && !rule?.canRequest ) {
-    return <ErrorNoAccess />
+  if (rule && !rule?.canRequest) {
+    return <ErrorNoAccess />;
   }
 
   return <AccessRequestForm rule={rule} ruleId={ruleId} />;
@@ -1126,30 +1124,30 @@ So I have just submitted the form directly using the submit button*/}
 const ErrorNoAccess = () => {
   return (
     <>
-        <Flex
-          height="100vh"
-          padding="0"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Stack textAlign="center" w="70%">
-            <Heading pb="10px">
-              <p>Oops, either this rule doesn't exist, </p>or you can't request
-              access to it
-            </Heading>
-            <Flex alignItems="center" justifyContent="center">
-              <ChakraLink
-                as={Link}
-                to={"/"}
-                transition="all .2s ease"
-                rounded="sm"
-                pt="75px"
-              >
-                <CommonFateLogo h="42px" w="auto" />
-              </ChakraLink>
-            </Flex>
-          </Stack>
-        </Flex>
-      </>
-  )
-}
+      <Flex
+        height="100vh"
+        padding="0"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Stack textAlign="center" w="70%">
+          <Heading pb="10px">
+            <p>Oops, either this rule doesn't exist, </p>or you can't request
+            access to it
+          </Heading>
+          <Flex alignItems="center" justifyContent="center">
+            <ChakraLink
+              as={Link}
+              to={"/"}
+              transition="all .2s ease"
+              rounded="sm"
+              pt="75px"
+            >
+              <CommonFateLogo h="42px" w="auto" />
+            </ChakraLink>
+          </Flex>
+        </Stack>
+      </Flex>
+    </>
+  );
+};
