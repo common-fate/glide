c6e689bb452a311c5cb0abde1679ba1d7d891f75
meyerjrr <jrmeyer339@gmail.com>
2023-01-12 10:59:34 +0800

    added check on users emails rather than username

--- accesshandler/pkg/providers/aws/sso-v2/validate.go
+++ accesshandler/pkg/providers/aws/sso-v2/validate.go
@@ -28,25 +28,34 @@ func (p *Provider) ValidateGrant() providers.GrantValidationSteps {
 				if err != nil {
 					return diagnostics.Error(err)
 				}
-				res, err := p.idStoreClient.ListUsers(ctx, &identitystore.ListUsersInput{
-					IdentityStoreId: aws.String(p.identityStoreID.Get()),
-					Filters: []types.Filter{{
-						AttributePath:  aws.String("UserName"),
-						AttributeValue: aws.String(subject),
-					}},
-				})
-				if err != nil {
-					return diagnostics.Error(err)
+
+				//Pull all users and check if emails match to find if user exists in AWS SSO
+				//This was required as filtering on Username, does not always work since some users do not use email as their username in AWS SSO
+				hasMore := true
+				var nextToken *string
+				for hasMore {
+
+					listUsers, err := p.idStoreClient.ListUsers(ctx, &identitystore.ListUsersInput{
+						IdentityStoreId: aws.String(p.identityStoreID.Get()),
+						NextToken:       nextToken,
+					})
+					if err != nil {
+						return diagnostics.Error(err)
+					}
+
+					for _, u := range listUsers.Users {
+						if *u.Emails[0].Value == subject {
+							return diagnostics.Info("User exists in SSO")
+
+						}
+					}
+
+					nextToken = listUsers.NextToken
+					hasMore = nextToken != nil
 				}
 
-				if len(res.Users) == 0 {
-					return diagnostics.Error(fmt.Errorf("could not find user %s in AWS SSO", subject))
-				}
-				if len(res.Users) > 1 {
-					// this should never happen, but check it anyway.
-					return diagnostics.Error(fmt.Errorf("expected 1 user but found %v", len(res.Users)))
-				}
-				return diagnostics.Info("User exists in SSO")
+				return diagnostics.Error(fmt.Errorf("could not find user %s in AWS SSO", subject))
+
 			},
 		},
 		"permission-set-should-exist": {
