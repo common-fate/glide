8cbb5e6cd4046da8fc2fb32db7983cb6e0895b5b
Manish Chaulagain <manish23chaulagain@gmail.com>
2023-02-01 15:48:14 +1100

    remove unnecessary files

--- tests/test.go
+++ tests/test.go
@@ -1,49 +1,51 @@
-package main
+// package main
 
-import (
-	"context"
-	"fmt"
+// import (
+// 	"context"
+// 	"fmt"
 
-	cachesvc "github.com/common-fate/common-fate/pkg/cachesync"
-	"github.com/common-fate/common-fate/pkg/deploy"
-	"github.com/common-fate/ddb"
-)
+// 	cachesvc "github.com/common-fate/common-fate/pkg/cachesync"
+// 	"github.com/common-fate/common-fate/pkg/deploy"
+// 	"github.com/common-fate/ddb"
+// )
 
-func run() error {
-	ctx := context.Background()
-	do, err := deploy.LoadConfig(deploy.DefaultFilename)
-	if err != nil {
-		return err
-	}
-	_, err = do.LoadOutput(ctx)
-	if err != nil {
-		return err
-	}
+// func run() error {
+// 	ctx := context.Background()
+// 	do, err := deploy.LoadConfig(deploy.DefaultFilename)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	_, err = do.LoadOutput(ctx)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	db, err := ddb.New(ctx, "common-fate-josh-7")
-	if err != nil {
-		return err
-	}
+// 	db, err := ddb.New(ctx, "common-fate-josh-7")
+// 	if err != nil {
+// 		return err
+// 	}
 
-	sync := cachesvc.CacheSyncer{
-		DB: db,
-	}
+// 	sync := cachesvc.CacheSyncer{
+// 		DB: db,
+// 	}
 
-	jsonPath := "/Users/eddie/dev/commonfate/commonfate/pkg/cachesync/schema.json"
-	err = sync.SyncCommunityProviderSchemasFromJSON(ctx, jsonPath)
-	if err != nil {
-		return err
-	}
+// 	fmt.Println("called")
 
-	fmt.Println("sync complete", sync)
+// 	jsonPath := "/Users/eddie/dev/commonfate/commonfate/pkg/cachesync/schema.json"
+// 	err = sync.SyncCommunityProviderSchemasFromJSON(ctx, jsonPath)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	fmt.Println("sync complete", sync)
 
-func main() {
-	err := run()
-	if err != nil {
-		fmt.Println("The error is", err)
-	}
+// 	return nil
+// }
 
-}
+// func main() {
+// 	err := run()
+// 	if err != nil {
+// 		fmt.Println("The error is", err)
+// 	}
+
+// }
