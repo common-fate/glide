6f4a0652f5c6aae7d43e7f519fc7a18ac123af43
meyerjrr <jrmeyer339@gmail.com>
2023-02-06 12:47:14 +1100

    comment out new services for now

--- pkg/service/targetdeploymentsvc/service.go
+++ pkg/service/targetdeploymentsvc/service.go
@@ -1,24 +1,16 @@
 package targetdeploymentsvc
 
-import (
-	"context"
-
-	"github.com/common-fate/common-fate/pkg/targetgroup"
-	"github.com/common-fate/ddb"
-	"k8s.io/utils/clock"
-)
-
 // Service holds business logic relating to Cognito user management.
-type Service struct {
-	Clock clock.Clock
-	DB    ddb.Storage
+// type Service struct {
+// 	Clock clock.Clock
+// 	DB    ddb.Storage
 
-	TargetDeployments TargetDeployments
-	AdminGroupID      string
-}
+// 	TargetDeployments TargetDeployments
+// 	AdminGroupID      string
+// }
 
-type TargetDeployments interface {
-	CreateDeployment(context.Context, CreateTargetDeploymentOpts) (targetgroup.Deployment, error)
-	UpdateDeployment(context.Context, UpdateTargetDeploymentOpts) (targetgroup.Deployment, error)
-	ArchiveDeployment(context.Context, ArchiveTargetDeploymentOpts) error
-}
+// type TargetDeployments interface {
+// 	CreateDeployment(context.Context, CreateTargetDeploymentOpts) (targetgroup.Deployment, error)
+// 	UpdateDeployment(context.Context, UpdateTargetDeploymentOpts) (targetgroup.Deployment, error)
+// 	ArchiveDeployment(context.Context, ArchiveTargetDeploymentOpts) error
+// }
--- pkg/service/targetgroupsvc/service.go
+++ pkg/service/targetgroupsvc/service.go
@@ -1,24 +1,16 @@
 package targetgroupsvc
 
-import (
-	"context"
-
-	"github.com/common-fate/common-fate/pkg/targetgroup"
-	"github.com/common-fate/ddb"
-	"k8s.io/utils/clock"
-)
-
 // Service holds business logic relating to Cognito user management.
-type Service struct {
-	Clock clock.Clock
-	DB    ddb.Storage
+// type Service struct {
+// 	Clock clock.Clock
+// 	DB    ddb.Storage
 
-	TargetDeployments TargetGroup
-	AdminGroupID      string
-}
+// 	TargetDeployments TargetGroup
+// 	AdminGroupID      string
+// }
 
-type TargetGroup interface {
-	CreateTargetGroup(context.Context, CreateTargetGroupOpts) (targetgroup.TargetGroup, error)
-	UpdateTargetGroup(context.Context, UpdateTargetGroupOpts) (targetgroup.TargetGroup, error)
-	ArchiveTargetGroup(context.Context, ArchiveTargetGroupOpts) error
-}
+// type TargetGroup interface {
+// 	CreateTargetGroup(context.Context, CreateTargetGroupOpts) (targetgroup.TargetGroup, error)
+// 	UpdateTargetGroup(context.Context, UpdateTargetGroupOpts) (targetgroup.TargetGroup, error)
+// 	ArchiveTargetGroup(context.Context, ArchiveTargetGroupOpts) error
+// }
