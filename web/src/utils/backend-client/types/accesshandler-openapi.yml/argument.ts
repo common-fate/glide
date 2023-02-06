dbfd8af62e93241809879b40997dcc4354e84700
Manish Chaulagain <manish23chaulagain@gmail.com>
2023-02-06 17:01:40 +1100

    update frontend to handle resourceName

--- web/src/utils/backend-client/types/accesshandler-openapi.yml/argument.ts
+++ web/src/utils/backend-client/types/accesshandler-openapi.yml/argument.ts
@@ -12,6 +12,7 @@ import type { ArgumentGroups } from './argumentGroups';
 export interface Argument {
   id: string;
   title: string;
+  resourceName?: string;
   description?: string;
   ruleFormElement: ArgumentRuleFormElement;
   /** Optional form element for the request form, if not provided, defaults to multiselect */
