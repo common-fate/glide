11ddc1cd9185a69da6ad5537d3ab4118b7fd15ee
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2023-02-08 16:30:31 +0800

    remove unneeded service and mock

--- pkg/service/targetdeploymentsvc/service.go
+++ pkg/service/targetdeploymentsvc/service.go
@@ -3,12 +3,10 @@ package targetdeploymentsvc
 import (
 	"github.com/benbjohnson/clock"
 	"github.com/common-fate/ddb"
-	registry_types "github.com/common-fate/provider-registry-sdk-go/pkg/providerregistrysdk"
 )
 
 // Service holds business logic relating to Cognito user management.
 type Service struct {
-	Clock                  clock.Clock
-	DB                     ddb.Storage
-	ProviderRegistryClient registry_types.ClientWithResponsesInterface
+	Clock clock.Clock
+	DB    ddb.Storage
 }
