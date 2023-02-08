5fc24ad43fe9f0796b9ff535a2f45f987ba4cae8
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2023-02-08 16:41:47 +0800

    add database lookup to router

--- pkg/service/requestroutersvc/service.go
+++ pkg/service/requestroutersvc/service.go
@@ -3,6 +3,7 @@ package requestroutersvc
 import (
 	"context"
 
+	"github.com/common-fate/common-fate/pkg/storage"
 	"github.com/common-fate/common-fate/pkg/targetgroup"
 	"github.com/common-fate/ddb"
 	"k8s.io/utils/clock"
@@ -12,19 +13,31 @@ type Service struct {
 	Clock clock.Clock
 	DB    ddb.Storage
 }
+
 // Route is a very basic router that just chooses the highest priority valid and healthy deployment
 // returns an error if none is found
 // has no way of falling back to lower priority
 func (s *Service) Route(ctx context.Context, tg targetgroup.TargetGroup) (*targetgroup.Deployment, error) {
-	var highest *targetgroup.DeploymentRegistration
-	for i, r := range tg.TargetDeployments {
-		if highest == nil || r.Priority > highest.Priority {
+	var priority int
+	var highest *targetgroup.Deployment
+	for _, r := range tg.TargetDeployments {
+		if highest == nil || r.Priority > priority {
 			if r.Valid {
-				// @TODO need to fetch the deployment from the database then check that it is healthy
-				// then it can be set as the highest
-				highest = &tg.TargetDeployments[i]
+				q := &storage.GetTargetGroupDeployment{
+					ID: r.ID,
+				}
+				_, err := s.DB.Query(ctx, q)
+				if err == ddb.ErrNoItems {
+					continue
+				}
+				if err != nil {
+					return nil, err
+				}
+				if q.Result.Healthy {
+					highest = &q.Result
+					priority = r.Priority
+				}
 			}
-
 		}
 	}
 	if highest == nil {
@@ -33,5 +46,5 @@ func (s *Service) Route(ctx context.Context, tg targetgroup.TargetGroup) (*targe
 
 	// s.DB.Query(ctx context.Context, qb ddb.QueryBuilder, opts ...func(*ddb.QueryOpts))
 	// @TODO lookup from database when queries are merged
-	return &targetgroup.Deployment{}, nil
+	return highest, nil
 }
--- pkg/storage/get_target_deployment.go
+++ pkg/storage/get_target_deployment.go
@@ -2,10 +2,12 @@ package storage
 
 import (
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/common-fate/common-fate/pkg/storage/keys"
 	"github.com/common-fate/common-fate/pkg/targetgroup"
+	"github.com/common-fate/ddb"
 )
 
 type GetTargetGroupDeployment struct {
@@ -15,6 +17,7 @@ type GetTargetGroupDeployment struct {
 
 func (g *GetTargetGroupDeployment) BuildQuery() (*dynamodb.QueryInput, error) {
 	qi := dynamodb.QueryInput{
+		Limit:                  aws.Int32(1),
 		KeyConditionExpression: aws.String("PK = :pk AND begins_with(SK, :sk)"),
 		ExpressionAttributeValues: map[string]types.AttributeValue{
 			":pk": &types.AttributeValueMemberS{Value: keys.TargetGroupDeployment.PK1},
@@ -22,4 +25,12 @@ func (g *GetTargetGroupDeployment) BuildQuery() (*dynamodb.QueryInput, error) {
 		},
 	}
 	return &qi, nil
+}
+
+func (g *GetTargetGroupDeployment) UnmarshalQueryOutput(out *dynamodb.QueryOutput) error {
+	if len(out.Items) != 1 {
+		return ddb.ErrNoItems
+	}
+
+	return attributevalue.UnmarshalMap(out.Items[0], &g.Result)
 }
