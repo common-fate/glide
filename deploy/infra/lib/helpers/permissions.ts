43d331a1d928cb53593d502ec7ad03f117045387
meyerjrr <jrmeyer339@gmail.com>
2023-02-14 10:29:53 +1100

    remove imports

--- deploy/infra/lib/helpers/permissions.ts
+++ deploy/infra/lib/helpers/permissions.ts
@@ -1,10 +1,4 @@
-import { Duration } from "aws-cdk-lib";
-import { Table } from "aws-cdk-lib/aws-dynamodb";
-import * as events from "aws-cdk-lib/aws-events";
-import * as targets from "aws-cdk-lib/aws-events-targets";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import { Construct } from "constructs";
-import * as path from "path";
 import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 export const grantInvokeCommunityProviders = (_lambda: lambda.Function) => {
