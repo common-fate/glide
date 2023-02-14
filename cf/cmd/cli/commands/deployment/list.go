d7ee2eea9ab3beaa7fde4540ffdb36b0b8c3b66b
jordiup <jordi.hermoso.up@gmail.com>
2023-02-14 11:46:40 +1100

    CF-667: List deployments cli command

---
+++ cf/cmd/cli/commands/deployment/list.go
@@ -0,0 +1,58 @@
+package deployment
+
+import (
+	"os"
+
+	"github.com/common-fate/clio"
+	"github.com/common-fate/common-fate/pkg/types"
+	"github.com/olekukonko/tablewriter"
+	"github.com/urfave/cli/v2"
+)
+
+var ListCommand = cli.Command{
+	Name:        "list",
+	Description: "list deployments",
+	Action: cli.ActionFunc(func(c *cli.Context) error {
+
+		opts := []types.ClientOption{}
+		ctx := c.Context
+
+		cfApi, err := types.NewClientWithResponses("http://0.0.0.0:8080", opts...)
+		if err != nil {
+			return err
+		}
+		res, err := cfApi.ListTargetGroupDeploymentsWithResponse(ctx)
+		if err != nil {
+			return err
+		}
+
+		if res.JSON200 != nil {
+			table := tablewriter.NewWriter(os.Stdout)
+			table.SetHeader([]string{"ID", "Account", "Region", "Health"})
+			table.SetAutoWrapText(false)
+			table.SetAutoFormatHeaders(true)
+			table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+			table.SetAlignment(tablewriter.ALIGN_LEFT)
+			table.SetCenterSeparator("")
+			table.SetColumnSeparator("")
+			table.SetRowSeparator("")
+			table.SetHeaderLine(false)
+			table.SetBorder(false)
+
+			for _, d := range res.JSON200.Res {
+				healthEmoji := "🟢"
+				if !d.Healthy {
+					healthEmoji = "🔴"
+				}
+				table.Append([]string{
+					d.Id, d.AwsAccount, d.AwsRegion, healthEmoji,
+				})
+			}
+			table.Render()
+		} else {
+			clio.Error("no deployments found")
+		}
+
+		return nil
+	}),
+}
