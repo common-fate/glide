55e7cb127dfc79acd15a8d17161d121975a199d2
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2023-02-17 08:26:34 +0800

    add .env export

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -359,6 +359,7 @@ func Dotenv() error {
 	myEnv["COMMONFATE_ACCESS_HANDLER_EXECUTION_ROLE_ARN"] = o.AccessHandlerExecutionRoleARN
 	myEnv["COMMONFATE_ACCESS_REMOTE_CONFIG_URL"] = cfg.Deployment.Parameters.ExperimentalRemoteConfigURL
 	myEnv["COMMONFATE_REMOTE_CONFIG_HEADERS"] = cfg.Deployment.Parameters.ExperimentalRemoteConfigHeaders
+	myEnv["COMMONFATE_GRANTER_V2_STATE_MACHINE_ARN"] = o.GranterV2StateMachineArn
 
 	err = godotenv.Write(myEnv, ".env")
 	if err != nil {
