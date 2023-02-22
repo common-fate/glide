7abb4bf065ca441e6dff95c6be75faec3a6ce25f
jordiup <jordi.hermoso.up@gmail.com>
2023-02-22 15:32:01 +1100

    Update onelogin_test.go

--- pkg/identity/identitysync/onelogin_test.go
+++ pkg/identity/identitysync/onelogin_test.go
@@ -1,9 +1,11 @@
 package identitysync
 
 import (
+	"context"
 	"reflect"
 	"testing"
 
+	"github.com/common-fate/common-fate/pkg/gconfig"
 	"github.com/common-fate/common-fate/pkg/identity"
 )
 
@@ -29,6 +31,22 @@ func Test_filterGroups(t *testing.T) {
 			},
 			want: []identity.IDPGroup{
 				{Name: "foo"},
+			},
+			wantErr: false,
+		},
+		{
+			name: "regex ends with: double match",
+			args: args{
+				groups: []identity.IDPGroup{
+					{Name: "foo"},
+					{Name: "oooooo"},
+					{Name: "bar"},
+				},
+				filterString: "oo$",
+			},
+			want: []identity.IDPGroup{
+				{Name: "foo"},
+				{Name: "oooooo"},
 			},
 			wantErr: false,
 		},
@@ -64,6 +82,52 @@ func Test_filterGroups(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("filterGroups() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestOneLoginSync_listGroupsWithFilter(t *testing.T) {
+	type fields struct {
+		clientID       gconfig.StringValue
+		clientSecret   gconfig.SecretStringValue
+		token          gconfig.SecretStringValue
+		idpGroupFilter gconfig.StringValue
+		baseURL        gconfig.StringValue
+	}
+	type args struct {
+		ctx          context.Context
+		filterString *string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []identity.IDPGroup
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &OneLoginSync{
+				clientID:       tt.fields.clientID,
+				clientSecret:   tt.fields.clientSecret,
+				token:          tt.fields.token,
+				idpGroupFilter: tt.fields.idpGroupFilter,
+				baseURL:        tt.fields.baseURL,
+			}
+
+			// @TODO: figure out how we can:
+			// mock the response from GET "/api/1/roles" http request for a http.NewRequest method
+
+			got, err := s.listGroupsWithFilter(tt.args.ctx, tt.args.filterString)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("OneLoginSync.listGroupsWithFilter() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("OneLoginSync.listGroupsWithFilter() = %v, want %v", got, tt.want)
 			}
 		})
 	}
