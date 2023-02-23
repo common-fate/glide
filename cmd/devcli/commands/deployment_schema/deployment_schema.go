9b4aca0c2cb94dde29ee7f208cc42be46e6420c2
jordiup <jordi.hermoso.up@gmail.com>
2023-02-23 11:17:06 +1100

    deployment schema: success/installation complete message

--- cmd/devcli/commands/deployment_schema/deployment_schema.go
+++ cmd/devcli/commands/deployment_schema/deployment_schema.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/common-fate/clio"
 	"github.com/common-fate/common-fate/pkg/deploy"
 	"github.com/invopop/jsonschema"
 	"github.com/urfave/cli/v2"
@@ -17,9 +18,6 @@ var DeploymentSchemaCommand = cli.Command{
 	Action: cli.ActionFunc(func(c *cli.Context) error {
 
 		r := new(jsonschema.Reflector)
-		// if err := r.AddGoComments("github.com/invopop/jsonschema", "./"); err != nil {
-		// 	return err
-		// }
 		s := r.Reflect(&deploy.Config{})
 
 		marshalled, err := s.MarshalJSON()
@@ -37,7 +35,25 @@ var DeploymentSchemaCommand = cli.Command{
 		if err != nil {
 			return err
 		}
-		fmt.Printf("wrote ./.vscode/schemas/commonfate-deployment-schema.json\n")
+		clio.Success("wrote ./.vscode/schemas/commonfate-deployment-schema.json\n")
+
+		clio.Successf(`To use this schema in vscode, add the following to your settings.json:\n\n
+"yaml.schemas": {
+	".vscode/schemas/commonfate-deployment-schema.json": "deployment.yml"
+},
+		`)
+
+		// TODO: we coudl also add auto installation for this in vscode settings
+		// err = os.MkdirAll("./.vscode", 0755)
+		// if err != nil {
+		// 	return err
+		// }
+		// // read first
+		// f, err := os.ReadFile("./.vscode/settings.json")
+		// if err != nil {
+		// 	return err
+		// }
+
 		return nil
 	}),
 }
