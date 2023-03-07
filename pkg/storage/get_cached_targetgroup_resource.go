4e5133606f169aaa7fa5ebb0944e416a593d685a
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2023-03-07 12:02:00 +0800

    fix rendering options for access request form

---
+++ pkg/storage/get_cached_targetgroup_resource.go
@@ -0,0 +1,38 @@
+package storage
+
+import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/common-fate/common-fate/pkg/cache"
+	"github.com/common-fate/common-fate/pkg/storage/keys"
+	"github.com/common-fate/ddb"
+)
+
+type GetCachedTargetGroupResource struct {
+	TargetGroupID string
+	ResourceType  string
+	ResourceID    string
+	Result        *cache.TargateGroupResource
+}
+
+func (q *GetCachedTargetGroupResource) BuildQuery() (*dynamodb.QueryInput, error) {
+	qi := dynamodb.QueryInput{
+		KeyConditionExpression: aws.String("PK = :pk1 and begins_with(SK, :sk1)"),
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":pk1": &types.AttributeValueMemberS{Value: keys.TargetGroupResource.PK1},
+			":sk1": &types.AttributeValueMemberS{Value: keys.TargetGroupResource.SK1(q.TargetGroupID, q.ResourceType, q.ResourceID)},
+		},
+	}
+	return &qi, nil
+}
+
+func (q *GetCachedTargetGroupResource) UnmarshalQueryOutput(out *dynamodb.QueryOutput) error {
+	if len(out.Items) == 0 {
+		return ddb.ErrNoItems
+	}
+
+	return attributevalue.UnmarshalMap(out.Items[0], &q.Result)
+
+}
