31e01fbff9f7b3e0841ba09aefcad70a3163e78f
meyerjrr <33018450+meyerjrr@users.noreply.github.com>
2023-03-07 11:19:33 +1100

    Fix tg list (#544)
    * updated names for v1-v2 fix empty data
    
    * use paginator

--- web/src/pages/admin/providersv2/index.tsx
+++ web/src/pages/admin/providersv2/index.tsx
@@ -35,8 +35,12 @@ import { usePaginatorApi } from "../../../utils/usePaginatorApi";
 export const ProvidersV2Tabs = () => {
   return (
     <ButtonGroup variant="ghost" spacing="0" mb={"-32px !important;"} my={4}>
-      <TabsStyledButton href="/admin/providers">V1</TabsStyledButton>
-      <TabsStyledButton href="/admin/providersv2">V2</TabsStyledButton>
+      <TabsStyledButton href="/admin/providers">
+        Built-In Providers
+      </TabsStyledButton>
+      <TabsStyledButton href="/admin/providersv2">
+        PDK Providers
+      </TabsStyledButton>
     </ButtonGroup>
   );
 };
@@ -175,7 +179,10 @@ const AdminProvidersTable = () => {
 };
 
 const AdminTargetGroupsTable = () => {
-  const { data } = useAdminListTargetGroups();
+  const paginator = usePaginatorApi<typeof useAdminListTargetGroups>({
+    swrHook: useAdminListTargetGroups,
+    hookProps: {},
+  });
   // @ts-ignore this is required because ts cannot infer the nexted object types correctly
   const cols: Column<TargetGroup>[] = useMemo(
     () => [
@@ -193,7 +200,7 @@ const AdminTargetGroupsTable = () => {
 
   return TableRenderer<TargetGroup>({
     columns: cols,
-    data: [],
+    data: paginator?.data?.targetGroups,
     emptyText: "No Target Groups have been set up yet.",
     linkTo: false,
   });
