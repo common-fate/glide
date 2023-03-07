5b55e5fe4e276b5e2ad923afcae63712dece17b1
meyerjrr <33018450+meyerjrr@users.noreply.github.com>
2023-03-07 19:21:29 +1100

    added better messaging and route checks to cache sync (#546)
    * added better messaging and route checks to cache sync
    
    * update error handling
    
    * fix log message
    
    ---------
    
    Co-authored-by: Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>

--- pkg/service/requestroutersvc/errors.go
+++ pkg/service/requestroutersvc/errors.go
@@ -2,4 +2,5 @@ package requestroutersvc
 
 import "errors"
 
-var ErrCannotRoute error = errors.New("cannot route to deployment for target group")
+var ErrCannotRoute error = errors.New("cannot route to a handler because all routes for this target group are invalid")
+var ErrNoRoutes error = errors.New("no routes exist for this target group")
--- web/src/pages/admin/providersv2/index.tsx
+++ web/src/pages/admin/providersv2/index.tsx
@@ -198,6 +198,7 @@ const AdminTargetGroupsTable = () => {
   return TableRenderer<TargetGroup>({
     columns: cols,
     data: paginator?.data?.targetGroups,
+    apiPaginator: paginator,
     emptyText: "No Target Groups have been set up yet.",
     linkTo: true,
   });
