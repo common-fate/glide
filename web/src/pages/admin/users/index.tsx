dd47ba6a98e5258bfbf81b1fae4f2c591a59b50c
jordiup <jordi.hermoso.up@gmail.com>
2023-03-09 17:03:24 +1100

    CF-803: frontend mock UI for user lookup

--- web/src/pages/admin/users/index.tsx
+++ web/src/pages/admin/users/index.tsx
@@ -1,14 +1,29 @@
-import { Container } from "@chakra-ui/react";
+import { Container, Input } from "@chakra-ui/react";
+import { useState } from "react";
 import { Helmet } from "react-helmet";
 import { AdminLayout } from "../../../components/Layout";
 import { UsersTable } from "../../../components/tables/UsersTable";
+import { useAdminListUsers } from "../../../utils/backend-client/admin/admin";
 
 const Index = () => {
+  // text state
+  const [text, setText] = useState("");
+
+  const userLookup = useAdminListUsers({
+    query: text,
+  });
+
   return (
     <AdminLayout>
       <Helmet>
         <title>Users</title>
       </Helmet>
+      <Input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      {JSON.stringify(userLookup.data)}
       <Container
         my={12}
         // This prevents unbounded widths for small screen widths
