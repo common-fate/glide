4a77559ce6c63a1bfa9a874f199dc8df8c8d2742
Manish Chaulagain <manish23chaulagain@gmail.com>
2023-03-15 20:45:07 +1100

    CF-831 Make slack notification less noisy (#577)

--- pkg/notifiers/slack/handle_grant.go
+++ pkg/notifiers/slack/handle_grant.go
@@ -53,9 +53,6 @@ func (n *SlackNotifier) HandleGrantEvent(ctx context.Context, log *zap.SugaredLo
 			},
 		}
 		fallback = fmt.Sprintf("Your access to %s is now active.", rq.Result.Name)
-	case gevent.GrantExpiredType:
-		msg = fmt.Sprintf("Your access to *%s* has now expired. If you still need access you can send another request using Common Fate.", rq.Result.Name)
-		fallback = fmt.Sprintf("Your access to %s has now expired.", rq.Result.Name)
 	case gevent.GrantFailedType:
 		msg = fmt.Sprintf("We've had an issue trying to provision or clean up your access to *%s*. We'll keep trying, but if you urgently need access to the role please contact your cloud administrator.", rq.Result.Name)
 		fallback = fmt.Sprintf("We've had an issue with your access to %s", rq.Result.Name)
