bcece26d4c9b96e9cc2afedca03da2fe19383541
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2023-04-18 12:47:26 +0800

    fix refresh cached resources
    Caching resources was not deleting old resources because teh query for existing items was malformed

--- pkg/storage/keys/targetgroup_resource.go
+++ pkg/storage/keys/targetgroup_resource.go
@@ -5,6 +5,7 @@ const TargetGroupResourceKey = "TARGET_GROUP_RESOURCE#"
 type targetGroupResourceKeys struct {
 	PK1                    string
 	SK1                    func(targetGroupID, resourceType string, value string) string
+	SK1TargetGroup         func(targetGroupID string) string
 	SK1TargetGroupResource func(targetGroupID, resourceType string) string
 }
 
@@ -12,6 +13,9 @@ var TargetGroupResource = targetGroupResourceKeys{
 	PK1: TargetGroupResourceKey,
 	SK1: func(targetGroupID, resourceType string, value string) string {
 		return targetGroupID + "#" + resourceType + "#" + value
+	},
+	SK1TargetGroup: func(targetGroupID string) string {
+		return targetGroupID + "#"
 	},
 	SK1TargetGroupResource: func(targetGroupID, resourceType string) string {
 		return targetGroupID + "#" + resourceType + "#"
