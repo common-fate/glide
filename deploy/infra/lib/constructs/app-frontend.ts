d0d6526ee3ab333d6489abdd9bb46479e4fdf14a
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2023-04-19 08:35:11 +0800

    bump package versions

--- deploy/infra/lib/constructs/app-frontend.ts
+++ deploy/infra/lib/constructs/app-frontend.ts
@@ -112,6 +112,7 @@ export class AppFrontend extends Construct {
     /* CDN */
 
     const accessLogBucket = new s3.Bucket(this, "AccessLogBucket", {
+      accessControl: undefined,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       encryption: s3.BucketEncryption.S3_MANAGED,
       objectOwnership: cdk.aws_s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
