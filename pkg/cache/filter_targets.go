bbb3d0810381e9a404ffe927d17109d080148161
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2023-05-04 12:39:17 +0800

    improve performance of filtering targets

--- pkg/cache/filter_targets.go
+++ pkg/cache/filter_targets.go
@@ -1,7 +1,6 @@
 package cache
 
 import (
-	"sort"
 	"sync"
 )
 
@@ -51,60 +50,19 @@ func (tf *FilterTargetsByAccessRule) Dump() []Target {
 	return values
 }
 
-// FilterTargetsByAccessRule can be used to filter paginated data
-type FilterTargetsByGroups struct {
-	groups map[string]struct{}
-	// mutex used to make concurrent writes safe to the output map
-	mu sync.Mutex
-	// using a map just to help with no duplicate values if you submit the same data for filtering twice
-	out map[string]Target
-}
-
-// AppendOutput is goroutine safe way to append to the output map
-func (tf *FilterTargetsByGroups) AppendOutput(target Target) {
-	tf.mu.Lock()
-	tf.out[target.ID()] = target
-	tf.mu.Unlock()
-}
-
-func NewFilterTargetsByGroups(groups []string) *FilterTargetsByGroups {
-	tf := FilterTargetsByGroups{
-		groups: make(map[string]struct{}),
-		out:    make(map[string]Target),
+func Filter(targets []Target, groups []string) []Target {
+	out := make([]Target, 0, len(targets))
+	groupsMap := make(map[string]struct{})
+	for _, v := range groups {
+		groupsMap[v] = struct{}{}
 	}
-	for i := range groups {
-		tf.groups[groups[i]] = struct{}{}
-	}
-	return &tf
-}
-
-func (tf *FilterTargetsByGroups) Filter(targets []Target) *FilterTargetsByGroups {
 	for _, target := range targets {
 		for group := range target.IDPGroupsWithAccess {
-			if _, ok := tf.groups[group]; ok {
-				tf.AppendOutput(target)
+			if _, ok := groupsMap[group]; ok {
+				out = append(out, target)
 				break
 			}
 		}
 	}
-	return tf
-}
-
-func (tf *FilterTargetsByGroups) Dump() []Target {
-	values := make([]Target, 0, len(tf.out))
-	for _, v := range tf.out {
-		values = append(values, v)
-	}
-	return values
-}
-func (tf *FilterTargetsByGroups) DumpSorted() []Target {
-	values := make([]Target, 0, len(tf.out))
-	for _, v := range tf.out {
-		values = append(values, v)
-	}
-	sort.Slice(values, func(i, j int) bool {
-		return values[i].ID() < values[j].ID()
-	})
-
-	return values
+	return out
 }
