e00b3253a8b93a99497cc9c0d62a5896fe80450c
meyerjrr <jrmeyer339@gmail.com>
2023-05-11 17:12:08 +1000

    added runtime getters

--- cmd/lambda/targetgroup-granter/handler.go
+++ cmd/lambda/targetgroup-granter/handler.go
@@ -9,11 +9,20 @@ import (
 	"github.com/common-fate/apikit/logger"
 	"github.com/common-fate/common-fate/pkg/config"
 	"github.com/common-fate/common-fate/pkg/gevent"
+	"github.com/common-fate/common-fate/pkg/handler"
 	"github.com/common-fate/common-fate/pkg/service/requestroutersvc"
 	"github.com/common-fate/common-fate/pkg/targetgroupgranter"
 	"github.com/common-fate/ddb"
+	"github.com/common-fate/provider-registry-sdk-go/pkg/handlerclient"
 	"github.com/sethvargo/go-envconfig"
 )
+
+type DefaultGetter struct {
+}
+
+func (DefaultGetter) GetRuntime(ctx context.Context, h handler.Handler) (*handlerclient.Client, error) {
+	return handler.GetRuntime(ctx, h)
+}
 
 func main() {
 	var cfg config.TargetGroupGranterConfig
@@ -38,7 +47,8 @@ func main() {
 		RequestRouter: &requestroutersvc.Service{
 			DB: db,
 		},
-		EventPutter: eventBus,
+		EventPutter:   eventBus,
+		RuntimeGetter: DefaultGetter{},
 	}
 	log, err := logger.Build(cfg.LogLevel)
 	if err != nil {
