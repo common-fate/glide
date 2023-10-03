5e75abb8dad566a6501ba26fa468c3a84b3c630b
Stuart Stone <stuartstone123@gmail.com>
2023-10-03 16:10:04 +0100

    Adds the ability to provide a filter to only sync users by a provided list of groups

--- pkg/identity/identitysync/google.go
+++ pkg/identity/identitysync/google.go
@@ -2,6 +2,7 @@ package identitysync
 
 import (
 	"context"
+	"strings"
 
 	"github.com/common-fate/common-fate/pkg/gconfig"
 	"github.com/common-fate/common-fate/pkg/identity"
@@ -16,6 +17,7 @@ type GoogleSync struct {
 	domain     gconfig.StringValue
 	adminEmail gconfig.StringValue
 	apiToken   gconfig.SecretStringValue
+	groups     gconfig.OptionalStringValue
 }
 
 func (s *GoogleSync) Config() gconfig.Config {
@@ -23,6 +25,7 @@ func (s *GoogleSync) Config() gconfig.Config {
 		gconfig.StringField("domain", &s.domain, "the Google domain"),
 		gconfig.StringField("adminEmail", &s.adminEmail, "the Google admin email"),
 		gconfig.SecretStringField("apiToken", &s.apiToken, "the Google API token", gconfig.WithNoArgs("/granted/secrets/identity/google/token"), gconfig.WithCLIPrompt(gconfig.CLIPromptTypeFile)),
+		gconfig.OptionalStringField("groups", &s.groups, "Groups that users are synced from."),
 	}
 }
 
@@ -75,7 +78,8 @@ func (c *GoogleSync) ListGroups(ctx context.Context) ([]identity.IDPGroup, error
 	return idpGroups, nil
 }
 
-func (c *GoogleSync) ListUsers(ctx context.Context) ([]identity.IDPUser, error) {
+func (c *GoogleSync) ListAllUsers(ctx context.Context) ([]identity.IDPUser, error) {
+
 	users := []identity.IDPUser{}
 	hasMore := true
 	var paginationToken string
@@ -97,7 +101,57 @@ func (c *GoogleSync) ListUsers(ctx context.Context) ([]identity.IDPUser, error) 
 		hasMore = paginationToken != ""
 	}
 	return users, nil
+}
 
+func (c *GoogleSync) ListUsersBasedOnGroups(ctx context.Context) ([]identity.IDPUser, error) {
+	idpUsers := []identity.IDPUser{}
+	var groupIds []string
+
+	if c.groups.Get() == "*" {
+		GoogleGroups, err := c.ListGroups(ctx)
+		if err != nil {
+			return nil, err
+		}
+		for _, GoogleGroup := range GoogleGroups {
+			groupIds = append(groupIds, GoogleGroup.ID)
+		}
+	} else {
+		groupIds = strings.Split(c.groups.Get(), ",")
+	}
+
+	for _, groupId := range groupIds {
+		hasMore := true
+		var paginationToken string
+		for hasMore {
+
+			userRes, err := c.client.Members.List(groupId).Do()
+			if err != nil {
+				return nil, err
+			}
+			for _, u := range userRes.Members {
+				userRes, err := c.client.Users.Get(u.Id).Do()
+				if err != nil {
+					return nil, err
+				}
+				user, err := c.idpUserFromGoogleUser(ctx, userRes)
+				if err != nil {
+					return nil, err
+				}
+				idpUsers = append(idpUsers, user)
+			}
+			paginationToken = userRes.NextPageToken
+			//Check that the next token is not nil so we don't need any more polling
+			hasMore = paginationToken != ""
+		}
+	}
+	return idpUsers, nil
+}
+
+func (c *GoogleSync) ListUsers(ctx context.Context) ([]identity.IDPUser, error) {
+	if c.groups.IsSet() {
+		return c.ListUsersBasedOnGroups(ctx)
+	}
+	return c.ListAllUsers(ctx)
 }
 
 // idpUserFromGoogleUser converts a Google user to the identityprovider interface user type
