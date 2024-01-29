160026547a07b921f6c8554a5616a7a05aac84c1
Hector Rivas Gandara <keymon@gmail.com>
2024-01-29 11:38:35 +0000

    delete-duplicated-users: allows filter by email

--- cmd/devcli/commands/ddb/delete_duplicated_users.go
+++ cmd/devcli/commands/ddb/delete_duplicated_users.go
@@ -5,13 +5,36 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/urfave/cli/v2"
 
 	"github.com/common-fate/common-fate/pkg/identity"
 	"github.com/common-fate/common-fate/pkg/storage"
 	"github.com/common-fate/common-fate/pkg/storage/ddbhelpers"
+	"github.com/common-fate/common-fate/pkg/storage/keys"
 	"github.com/common-fate/ddb"
 )
+
+// Needed to filter by duplicates. Not needed in normal operations
+type ListUsersForEmail struct {
+	Result []identity.User `ddb:"result"`
+	Email  string
+}
+
+func (l *ListUsersForEmail) BuildQuery() (*dynamodb.QueryInput, error) {
+
+	qi := dynamodb.QueryInput{
+		IndexName:              aws.String(keys.IndexNames.GSI2),
+		KeyConditionExpression: aws.String("GSI2PK = :pk2 and GSI2SK = :sk2"),
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":pk2": &types.AttributeValueMemberS{Value: keys.Users.GSI2PK},
+			":sk2": &types.AttributeValueMemberS{Value: keys.Users.GSI2SK(string(l.Email))},
+		},
+	}
+	return &qi, nil
+}
 
 func findDuplicates(users []identity.User, maxDups int) []identity.User {
 	toDelete := []identity.User{}
@@ -37,21 +60,21 @@ func findDuplicates(users []identity.User, maxDups int) []identity.User {
 
 var deleteDuplicatedUsersCommand = cli.Command{
 	Name: "delete-duplicated-users",
-	Description: `
-		Find any duplicated users in the Dynamodb DB.
+	Description: `Find any duplicated users in the Dynamodb DB.
 
-		This command fixes the issue of duplicated users added during sync due
-		the lack of pagination querying dynamodb.
+This command fixes the issue of duplicated users added during sync due
+the lack of pagination querying dynamodb.
 
-		It will list all users in dynamodb, identifying those with a duplicated
-		email, and keeping the one with the newest creation date.
+It will list all users in dynamodb, identifying those with a duplicated
+email, and keeping the one with the newest creation date.
 
-		Runs by default in dry-run mode, or allows to delete them from dynamodb.
+Runs by default in dry-run mode, or allows to delete them from dynamodb.
 	`,
 	Flags: []cli.Flag{
 		&cli.StringFlag{Name: "name", Usage: "The name of the table", Required: true},
 		// TODO set the region? now I user AWS_REGION=...
 		// &cli.StringFlag{Name: "region", Aliases: []string{"r"}, Usage: "AWS region to provision the table into"},
+		&cli.StringFlag{Name: "email", Aliases: []string{"r"}, Usage: "Search duplicates only for given email"},
 		&cli.IntFlag{Name: "limit", Usage: "Total limit of elements to stop pagination. Set 0 for unlimited.", Value: 0},
 		&cli.BoolFlag{Name: "pagination", Usage: "Process pagination in query", Value: true},
 		&cli.BoolFlag{Name: "dry-run", Usage: "Set to false to actually delete the users. Default true", Value: true},
@@ -61,6 +84,7 @@ var deleteDuplicatedUsersCommand = cli.Command{
 	Action: func(c *cli.Context) error {
 		ctx := c.Context
 		tableName := c.String("name")
+		email := c.String("email")
 		limit := c.Int("limit")
 		pagination := c.Bool("pagination")
 		dryRun := c.Bool("dry-run")
@@ -71,7 +95,12 @@ var deleteDuplicatedUsersCommand = cli.Command{
 			return err
 		}
 
-		uq := &storage.ListUsers{}
+		var uq ddb.QueryBuilder
+		if email == "" {
+			uq = &storage.ListUsers{}
+		} else {
+			uq = &ListUsersForEmail{Email: email}
+		}
 
 		users := []identity.User{}
 		err = ddbhelpers.QueryPages(ctx, db, uq,
@@ -80,6 +109,8 @@ var deleteDuplicatedUsersCommand = cli.Command{
 				case *storage.ListUsersForStatus:
 					users = append(users, qb.Result...)
 				case *storage.ListUsers:
+					users = append(users, qb.Result...)
+				case *ListUsersForEmail:
 					users = append(users, qb.Result...)
 				default:
 					panic("Unknown type for Query Buidler")
